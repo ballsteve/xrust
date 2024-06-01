b35d3a9ed7f92510c346f4335d0419c340224582
Daniel Murphy <daniel@devasta.ie>
2024-06-01 21:56:44 +0100

    Parser bugfixes.

--- src/parser/xml/attribute.rs
+++ src/parser/xml/attribute.rs
@@ -16,6 +16,7 @@ use crate::parser::{ParseError, ParseInput};
 use crate::value::Value;
 use std::collections::HashMap;
 use std::rc::Rc;
+use crate::qname::QualifiedName;
 
 pub(crate) fn attributes<N: Node>(
 ) -> impl Fn(ParseInput<N>) -> Result<(ParseInput<N>, Vec<N>), ParseError> {
@@ -23,37 +24,37 @@ pub(crate) fn attributes<N: Node>(
         Ok(((input1, mut state1), nodes)) => {
             let n: HashMap<String, String> = HashMap::new();
             let mut namespaces = state1.namespace.last().unwrap_or(&n).clone();
-            for node in nodes.clone() {
+            for (qn, val) in nodes.clone() {
                 //Return error if someone attempts to redefine namespaces.
-                if (node.name().get_prefix() == Some("xmlns".to_string()))
-                    && (node.name().get_localname() == *"xmlns")
+                if (qn.get_prefix() == Some("xmlns".to_string()))
+                    && (qn.get_localname() == *"xmlns")
                 {
                     return Err(ParseError::NotWellFormed(String::from(
                         "cannot redefine namespace",
                     )));
                 }
                 //xml prefix must always be set to http://www.w3.org/XML/1998/namespace
-                if (node.name().get_prefix() == Some("xmlns".to_string()))
-                    && (node.name().get_localname() == *"xml")
-                    && (node.to_string() != *"http://www.w3.org/XML/1998/namespace")
+                if (qn.get_prefix() == Some("xmlns".to_string()))
+                    && (qn.get_localname() == *"xml")
+                    && (val != "http://www.w3.org/XML/1998/namespace".to_string())
                 {
                     return Err(ParseError::NotWellFormed(String::from(
                         "xml namespace URI must be http://www.w3.org/XML/1998/namespace",
                     )));
                 }
                 // http://www.w3.org/XML/1998/namespace must always be bound to xml
-                if (node.name().get_prefix() == Some("xmlns".to_string()))
-                    && (node.name().get_localname() != *"xml")
-                    && (node.to_string() == *"http://www.w3.org/XML/1998/namespace")
+                if (qn.get_prefix() == Some("xmlns".to_string()))
+                    && (qn.get_localname() != *"xml")
+                    && (val == "http://www.w3.org/XML/1998/namespace".to_string())
                 {
                     return Err(ParseError::NotWellFormed(String::from(
                         "XML namespace must be bound to xml prefix",
                     )));
                 }
                 // http://www.w3.org/2000/xmlns/ must always be bound to xmlns
-                if (node.name().get_prefix() == Some("xmlns".to_string()))
-                    && (node.name().get_localname() != *"xmlns")
-                    && (node.to_string() == *"http://www.w3.org/2000/xmlns/")
+                if (qn.get_prefix() == Some("xmlns".to_string()))
+                    && (qn.get_localname() != *"xmlns")
+                    && (val == "http://www.w3.org/2000/xmlns/".to_string())
                 {
                     return Err(ParseError::NotWellFormed(String::from(
                         "XMLNS namespace must be bound to xmlns prefix",
@@ -61,10 +62,10 @@ pub(crate) fn attributes<N: Node>(
                 }
                 // Default namespace cannot be http://www.w3.org/XML/1998/namespace
                 // Default namespace cannot be http://www.w3.org/2000/xmlns/
-                if (node.name().get_prefix().is_none())
-                    && (node.name().get_localname() == *"xmlns")
-                    && (node.to_string() == *"http://www.w3.org/XML/1998/namespace"
-                        || node.to_string() == *"http://www.w3.org/2000/xmlns/")
+                if (qn.get_prefix().is_none())
+                    && (qn.get_localname() == *"xmlns")
+                    && (val == *"http://www.w3.org/XML/1998/namespace"
+                        || val == *"http://www.w3.org/2000/xmlns/")
                 {
                     return Err(ParseError::NotWellFormed(String::from(
                         "invalid default namespace",
@@ -72,9 +73,9 @@ pub(crate) fn attributes<N: Node>(
                 }
 
                 // XML 1.0 documents cannot redefine an alias to ""
-                if (node.name().get_prefix() == Some("xmlns".to_string()))
-                    && (node.name().get_localname() != "")
-                    && (node.to_string() == *"")
+                if (qn.get_prefix() == Some("xmlns".to_string()))
+                    && (qn.get_localname() != "")
+                    && (val.to_string() == *"")
                     && state1.xmlversion == *"1.0"
                 {
                     return Err(ParseError::NotWellFormed(String::from(
@@ -82,17 +83,17 @@ pub(crate) fn attributes<N: Node>(
                     )));
                 }
 
-                if (node.name().get_prefix() == Some("xmlns".to_string()))
-                    || (node.name().get_localname() == *"xmlns")
+                if (qn.get_prefix() == Some("xmlns".to_string()))
+                    || (qn.get_localname() == *"xmlns")
                 {
-                    namespaces.insert(node.name().get_localname(), node.to_string());
+                    namespaces.insert(qn.get_localname(), val.to_string());
                 };
 
                 //Check if the xml:space attribute is present and if so, does it have
                 //"Preserved" or "Default" as its value. We'll actually handle in a future release.
-                if node.name().get_prefix() == Some("xml".to_string())
-                    && node.name().get_localname() == *"space"
-                    && !(node.to_string() == "Default" || node.to_string() == "Preserve")
+                if qn.get_prefix() == Some("xml".to_string())
+                    && qn.get_localname() == *"space"
+                    && !(qn.to_string() == "Default" || qn.to_string() == "Preserve")
                 {
                     return Err(ParseError::Validation {
                         row: state1.currentrow,
@@ -107,28 +108,37 @@ pub(crate) fn attributes<N: Node>(
             // Then loop through the prefixed attributes after the namespaces have been processed
             let mut resnodes = vec![];
             let mut resnodenames = vec![];
-            for node in nodes {
-                if node.name().get_prefix() != Some("xmlns".to_string())
-                    && node.name().get_localname() != *"xmlns"
+            for (mut qn, attrval) in nodes {
+                if qn.get_prefix() != Some("xmlns".to_string())
+                    && qn.get_localname() != *"xmlns"
                 {
-                    //if let Some(ns) = node.name().get_prefix() {
-                    //    if ns == *"xml" {
-                    //        node.set_nsuri("http://www.w3.org/XML/1998/namespace".to_string())
-                    //    } else {
-                    //        match namespaces.get(&*ns) {
-                    //            None => return Err(ParseError::MissingNameSpace),
-                    //            Some(nsuri) => node.set_nsuri(nsuri.clone()),
-                    //        }
-                    //    }
-                    //}
+                    if let Some(ns) = qn.get_prefix() {
+                        if ns == *"xml" {
+                            let _ = qn.resolve(&vec!(HashMap::from([("xml".to_string(),"http://www.w3.org/XML/1998/namespace".to_string())])));
+                        } else {
+                            let _ = qn.resolve(&state1.namespace);
+                            if qn.get_nsuri() == None {
+                                return Err(ParseError::MissingNameSpace)
+                            }
+                        }
+                    }
+
+                    let newatt = state1
+                        .doc
+                        .clone()
+                        .unwrap()
+                        .new_attribute(qn.clone(), Rc::new(Value::String(attrval)))
+                        .expect("unable to create attribute");
+                    resnodes.push(newatt);
+
                     /* Why not just use resnodes.contains()  ? I don't know how to do partial matching */
                     if resnodenames
-                        .contains(&(node.name().get_nsuri(), node.name().get_localname()))
+                        .contains(&(qn.get_nsuri(), qn.get_localname()))
                     {
                         return Err(ParseError::NotWellFormed(String::from("missing namespace")));
                     } else {
-                        resnodenames.push((node.name().get_nsuri(), node.name().get_localname()));
-                        resnodes.push(node);
+                        resnodenames.push((qn.get_nsuri(), qn.get_localname()));
+
                     }
                 }
             }
@@ -138,7 +148,7 @@ pub(crate) fn attributes<N: Node>(
     }
 }
 // Attribute ::= Name '=' AttValue
-fn attribute<N: Node>() -> impl Fn(ParseInput<N>) -> Result<(ParseInput<N>, N), ParseError> {
+fn attribute<N: Node>() -> impl Fn(ParseInput<N>) -> Result<(ParseInput<N>, (QualifiedName, String)), ParseError> {
     move |(input, state)| match tuple6(
         whitespace1(),
         qualname(),
@@ -150,11 +160,7 @@ fn attribute<N: Node>() -> impl Fn(ParseInput<N>) -> Result<(ParseInput<N>, N), 
     {
         Ok(((input1, state1), (_, n, _, _, _, s))) => Ok((
             (input1, state1.clone()),
-            state1
-                .doc
-                .unwrap()
-                .new_attribute(n, Rc::new(Value::String(s)))
-                .expect("unable to create attribute"),
+            (n, s)
         )),
         Err(e) => Err(e),
     }
