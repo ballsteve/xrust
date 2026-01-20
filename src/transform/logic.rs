d8a65651712cc782d95f4629cecbe2e2edcf93ce
Steve Ball <Steve.Ball@explain.com.au>
2026-01-20 16:54:35 +1100

    Don't panic if comparison fails

--- src/transform/logic.rs
+++ src/transform/logic.rs
@@ -76,8 +76,7 @@ pub(crate) fn general_comparison<
     let mut b = false;
     for i in left {
         for j in &right {
-            eprintln!("comparing {:?} with {:?} operator {:?}", i, j, o);
-            b = i.compare(j, *o).unwrap();
+            b = i.compare(j, *o)?;
             if b {
                 break;
             }
